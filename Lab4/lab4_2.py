ec748dd2f1ddd6ee3f2067a6d75356f8a6d12dcb
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-10-25 17:10:44 +0200

    cryptosystem entropy and probabilities

--- Lab4/lab4_2.py
+++ Lab4/lab4_2.py
@@ -1,50 +1,42 @@
 import numpy as np
 
-prob_P = {'a': 1/2, 'b': 1/3, 'c': 1/6}
+# Prawdopodobieństwo tekstu jawnego
+prob_P = {'a': 1 / 2, 'b': 1 / 3, 'c': 1 / 6}
 
 def entropy(prob_dist):
-    return -sum(p * np.log2(p) for p in prob_dist.values())
+    return -sum(p * np.log2(p) for p in prob_dist.values() if p > 0)
 
 H_P = entropy(prob_P)
 
-prob_K = {'K1': 1/3, 'K2': 1/3, 'K3': 1/3}
-
+# Prawdopodobieństwo klucza
+prob_K = {'K1': 1 / 3, 'K2': 1 / 3, 'K3': 1 / 3}
 H_K = entropy(prob_K)
 
+# Macierz szyfrowania i prawdopodobieństwo szyfrogramu
 encryption_matrix = {
     'K1': {'a': 1, 'b': 2, 'c': 3},
     'K2': {'a': 2, 'b': 3, 'c': 4},
     'K3': {'a': 3, 'b': 4, 'c': 1}
 }
 
-cipher_values = {1, 2, 3, 4}
-
+# Prawdopodobieństwo dla każdego szyfrogramu
 prob_C = {1: 0, 2: 0, 3: 0, 4: 0}
-
 for key, mapping in encryption_matrix.items():
     for plaintext, cipher in mapping.items():
         prob_C[cipher] += prob_P[plaintext] * prob_K[key]
 
 H_C = entropy(prob_C)
 
-joint_prob_KC = {}
+# H(K|C) = H(K) + H(P) − H(C)
+H_K_given_C = H_K + H_P - H_C
 
-for key, mapping in encryption_matrix.items():
-    for plaintext, cipher in mapping.items():
-        joint_prob_KC[(key, cipher)] = prob_P[plaintext] * prob_K[key]
-
-H_KC = -sum(p * np.log2(p) for p in joint_prob_KC.values())
-
-H_K_given_C = H_KC - H_C
-
+# Wzor entropii warunkowej H(P|C)
 joint_prob_PC = {}
-
 for key, mapping in encryption_matrix.items():
     for plaintext, cipher in mapping.items():
         joint_prob_PC[(plaintext, cipher)] = prob_P[plaintext] * prob_K[key]
 
-H_PC = -sum(p * np.log2(p) for p in joint_prob_PC.values())
-
+H_PC = -sum(p * np.log2(p) for p in joint_prob_PC.values() if p > 0)
 H_P_given_C = H_PC - H_C
 
 print(f"H_P = {H_P}")
